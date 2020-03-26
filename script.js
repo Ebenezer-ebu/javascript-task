65a147b4171f384f57de76f4f2dbf34149dd5c96
ebenezer ebu <eifezulike@gmail.com>
2020-03-26 22:47:26 +0100

    first-commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let myName = "Ebenezer Ebuka Ifezulike";
+
+const courses = ["html","css", "javascript", "c-Sharp", "Go"," php", "nodejs"];
+
+console.log("My name is " + myName);
+console.log("My courses are " + courses);
+
+let data = [];
+for(let i = 0; i <= 200; i++) {
+  if (i % 2 !== 0) {
+    data.push(i); 
+  }   
+}
+console.log(data);
